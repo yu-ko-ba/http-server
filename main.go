4e7ea705204f320ec09240a6a203cc66ef7d6dcd
yu-ko-ba <yu-ko-ba@outlook.com>
2022-01-27 11:28:02 +0900

    IPアドレスを表示するようにした

--- main.go
+++ main.go
@@ -2,13 +2,33 @@ package main
 
 import (
 	"flag"
-	"strconv"
+	"fmt"
+	"log"
+	"net"
 	"net/http"
+	"strconv"
+	"strings"
 )
 
 func main() {
-    port := flag.Int("port", 8080, "port number")
+    addrs, err := net.InterfaceAddrs()
+    if err != nil {
+        log.Fatalln(err)
+    }
+
+    for _, addr := range addrs {
+        a, ok := addr.(*net.IPNet)
+        if ok && a.IP.To4() != nil && !strings.Contains(a.String(), "127.0.0.1") {
+            fmt.Println("IP address : " + a.String())
+        }
+    }
+
+    portNumber := flag.Int("port", 8080, "port number")
     flag.Parse()
 
-    http.ListenAndServe(":" + strconv.Itoa(*port), http.FileServer(http.Dir(flag.Arg(0))))
+    port := strconv.Itoa(*portNumber)
+
+    fmt.Println("Port number: " + port)
+
+    http.ListenAndServe(":" + port, http.FileServer(http.Dir(flag.Arg(0))))
 }
